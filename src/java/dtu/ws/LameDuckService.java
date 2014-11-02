228fcb852324a0cee7d26f82a8cf15c27ea06c52
Rasmus Lundsgaard <s123344@student.dtu.dk>
2014-11-03 00:06:59 +0100

    initial commit. Very simple webservice in desperate need of soap

---
+++ src/java/dtu/ws/LameDuckService.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dtu.ws;
+
+
+/**
+ *
+ * @author rasmus
+ */
+@javax.jws.WebService
+public class LameDuckService {
+    public String getFlights(String departureAirport, String destinationAirport, String departureDate){
+        return "Hello ";
+    }
+    
+    public void bookFlight(){
+        
+    }
+    
+    public void cancelFlight(){
+        
+    }
+}
